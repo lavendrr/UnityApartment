0e610ebb6aee703eb4ae943006c85af9c09269d9
Ricardo M. <rmoctezumaf@gmail.com>
2025-10-14 20:48:21 -0500

    Added Faucet.cs & OnUse receiver

---
+++ Apartment/Assets/Scripts/Objects/Faucet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Faucet : MonoBehaviour
+{
+    
+    void OnUse()
+    {
+        Debug.Log("used!");
+    }
+}
