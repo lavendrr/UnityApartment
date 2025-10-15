47ecff962e6a7021959a1f0981f13edfef948622
Ricardo M. <rmoctezumaf@gmail.com>
2025-10-15 16:15:52 -0500

    Faucet handle is draggable

--- Apartment/Assets/Scripts/Objects/Faucet.cs
+++ Apartment/Assets/Scripts/Objects/Faucet.cs
@@ -4,9 +4,44 @@ using UnityEngine;
 
 public class Faucet : MonoBehaviour
 {
-    
+    private bool beingDragged = false;
+    private GameObject player;
+    private Vector3 ?positionDelta;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        if (beingDragged)
+        {
+            Pull(player.transform);
+        }
+    }
+
     void OnUse()
     {
-        Debug.Log("used!");
+        if (beingDragged)
+        {
+            beingDragged = false;
+            Debug.Log("stopped dragging!");
+            positionDelta = null;
+        }
+        else
+        {
+            beingDragged = true;
+            Debug.Log("started dragging!");
+        }
+    }
+    
+    void Pull(Transform playerTransform)
+    {
+        if (positionDelta == null)
+        {
+            positionDelta = playerTransform.position - transform.position;
+        }
+        transform.position = playerTransform.position - (Vector3)positionDelta;
     }
 }
