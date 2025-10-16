4375de0bbef6806b7105950432e795e255182896
Ricardo M. <rmoctezumaf@gmail.com>
2025-10-15 19:24:12 -0500

    Linked water VFX to faucet handle position

--- Apartment/Assets/Scripts/Objects/Faucet.cs
+++ Apartment/Assets/Scripts/Objects/Faucet.cs
@@ -10,28 +10,42 @@ public class Faucet : MonoBehaviour
     private Transform originalParent;
     private Vector3? positionDelta;
     private Vector3 restingPosition;
+    private ParticleSystem waterVFX;
 
     void Start()
     {
         player = GameObject.Find("Player");
         originalParent = transform.parent;
         restingPosition = transform.position;
+        waterVFX = GameObject.Find("WaterVFX").GetComponent<ParticleSystem>();
     }
 
     void Update()
     {
         if (beingDragged)
         {
+            bool playVFX = true;
             Vector3 newPosition = new Vector3(transform.position.x, restingPosition.y, restingPosition.z);
             // Pull(player.transform);
             if (transform.position.x <= restingPosition.x)
             {
                 newPosition.x = restingPosition.x;
+                playVFX = false;
             }
             else if (transform.position.x > restingPosition.x + 0.025)
             {
                 newPosition.x = (float)(restingPosition.x + 0.025);
             }
+
+            if (playVFX && !waterVFX.isPlaying)
+            {
+                waterVFX.Play();
+            }
+            else if (!playVFX && waterVFX.isPlaying)
+            {
+                waterVFX.Stop();
+            }
+
             Quaternion angle = Quaternion.AngleAxis((newPosition.x - restingPosition.x) * 1200, new Vector3(1, 0, 0));
             Quaternion angle2 = Quaternion.AngleAxis(Mathf.Clamp(transform.parent.position.z - newPosition.z, -1f, 1f) * 45f, new Vector3(0, 0, 1));
             Quaternion angle3 = angle * angle2;
